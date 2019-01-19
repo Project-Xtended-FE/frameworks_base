973e73ee47307871157cfc40b6a25612266326cb
Alex Cruz <du.alexcruz@gmail.com>
2019-01-19 17:30:24 -0600

    SystemUI: [SQUASHED] Ambient display show battery & cpu temperature [1/2]
    - Allow to hide battery in doze
    - Add a method to determine CPU temperature, battery temp
    
    Co-authored-by: Aston-Martinn <advaithbhat9@gmail.com>
    Co-authored-by: eyosen <abittin@gmail.com>
    Change-Id: I2c55f607a9ad1a9693618d5f23ff65a2a203efa1
    Signed-off-by: Ghosuto <clash.raja10@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -7430,6 +7430,22 @@ public final class Settings {
         public static final String QQS_TILES_ROWS = "qqs_tiles_rows";
 
         /**
+         * What to show at the bottom of the Ambient display
+         * 0: Nothing.
+         * 1: Battery Percentage.
+         * 2: Battery Temperature.
+         * 3: Battery Percentage & Temperature Together.
+         * @hide
+         */
+        public static final String AMBIENT_SHOW_SETTINGS = "ambient_show_settings";
+
+        /**
+         * Ambient settings show icons
+         * @hide
+         */
+        public static final String AMBIENT_SHOW_SETTINGS_ICONS = "ambient_show_settings_icons";
+
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *
