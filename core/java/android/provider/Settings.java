25f3ca8d2db7136148649a618df4d4670539eb0c
SagarMakhar <sagarmakhar@gmail.com>
2021-12-21 15:34:11 +0000

    Bring back UDFPS animation [SQUASHED]
    neobuddy89: Adapted for Android 12L, various fixes for A15, A16
    Stallix - Adapted for A13 QPR1
    
    Co-authored-by: Pranav Vashi <neobuddy89@gmail.com>
    Co-authored-by: Dhina17 <dhinalogu@gmail.com>
    Co-authored-by: AnierinB <anierin@evolution-x.org>
    Co-authored-by: minaripenguin <minaripenguin@users.noreply.github.com>
    Co-authored-by: jhonboy121 <alfredmathew05@gmail.com>
    Co-authored-by: daniml3 <danimoral1001@gmail.com>
    Co-authored-by: DennySPb <dennyspb@gmail.com>
    Co-authored-by: kondors1995 <normandija1945@gmail.com>
    Co-authored-by: jhenrique09 <jhenrique09.mcz@hotmail.com>
    Co-authored-by: Hikari-no-Tenshi <Hikari-no-Tenshi@users.noreply.github.com>
    Co-authored-by: TheScarastic <warabhishek@gmail.com>
    Co-authored-by: nullxception <nullxception@users.noreply.github.com>
    Co-authored-by: Joey Huab <joey@evolution-x.org>
    Co-authored-by: 00day0 <00day0@users.noreply.github.com>
    Co-authored-by: flakeforever <113103193+flakeforever@users.noreply.github.com>
    Change-Id: I12ca669685a6183a853f96e08ea516d2d673ebc9
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: Ghosuto <clash.raja10@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -7531,6 +7531,11 @@ public final class Settings {
         public static final String WIRELESS_CHARGING_DYNAMIC_COLOR = "wireless_charging_dynamic_color";
 
         /**
+         * @hide
+         */
+        public static final String UDFPS_ANIM_STYLE = "udfps_anim_style";
+
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *
