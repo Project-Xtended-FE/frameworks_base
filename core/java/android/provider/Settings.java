1051a87fe55a32b93d9a519b9c8cef3791bce015
Pranav Vashi <neobuddy89@gmail.com>
2022-01-17 21:20:04 +0530

    SystemUI: Allow toggling privacy indicators [1/2]
    Co-authored-by: jhonboy121 <alfredmathew05@gmail.com>
    Co-authored-by: Ido Ben-Hur <idoybh2@gmail.com>
    Change-Id: Iad8b768e1890957a9efde485259b27e8f1dd1963
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: AnierinB <anierin@evolution-x.org>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -12925,6 +12925,24 @@ public final class Settings {
          */
         @Readable
         public static final String KEYBOX_DATA = "keybox_data";
+       
+        /**
+        * Whether to show privacy indicator for location
+         * @hide
+         */
+        public static final String ENABLE_LOCATION_PRIVACY_INDICATOR = "enable_location_privacy_indicator";
+
+        /**
+         * Whether to show privacy indicator for camera
+         * @hide
+         */
+        public static final String ENABLE_CAMERA_PRIVACY_INDICATOR = "enable_camera_privacy_indicator";
+
+        /**
+         * Whether to show privacy indicator for media projection
+         * @hide
+         */
+        public static final String ENABLE_PROJECTION_PRIVACY_INDICATOR = "enable_projection_privacy_indicator";
 
         /**
          * Store vboot key.
