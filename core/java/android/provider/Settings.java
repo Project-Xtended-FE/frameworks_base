a5656c5fa33652097e74c95ad5f24aaf1462d295
HELLBOY017 <abhaygill017@gmail.com>
2022-06-12 17:42:59 +0530

    base: Add switch for small landscape notifications [1/2]
    Change-Id: Iab534b2f66eba2f162654c614867a3ef63a1563f
    Signed-off-by: Ghosuto <clash.raja10@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -7393,6 +7393,12 @@ public final class Settings {
         public static final String DOUBLE_TAP_SLEEP_LOCKSCREEN = "double_tap_sleep_lockscreen";
 
         /**
+         * Whether to enable small size landcape notifications or not
+         * @hide
+         */
+        public static final String SMALL_LANDSCAPE_NOTIFICATION = "small_landscape_notification";
+
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *
