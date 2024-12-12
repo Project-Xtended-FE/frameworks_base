130b7b0f9f38e44926cf8d557671a1bd6bd1448e
minaripenguin <minaripenguin@users.noreply.github.com>
2024-12-12 13:22:43 +0800

    Themes: Add support for progress bar styles
    Change-Id: I9fee689fe6a8c08455510d71e76d5bece27e85cd
    Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>

--- packages/SystemUI/src/com/android/systemui/theme/ThemeOverlayApplier.java
+++ packages/SystemUI/src/com/android/systemui/theme/ThemeOverlayApplier.java
@@ -134,6 +134,12 @@ public class ThemeOverlayApplier implements Dumpable {
     @VisibleForTesting
     static final String OVERLAY_CATEGORY_POWER_MENU =
             "android.theme.customization.powermenu";
+    @VisibleForTesting
+    static final String OVERLAY_CATEGORY_LOCK_CLOCK_FONT =
+            "android.theme.customization.lockscreen_clock_font";
+    @VisibleForTesting
+    static final String OVERLAY_CATEGORY_PROGRESS_BAR =
+            "android.theme.customization.progress_bar";
 
     /*
      * All theme customization categories used by the system, in order that they should be applied,
@@ -151,7 +157,8 @@ public class ThemeOverlayApplier implements Dumpable {
             OVERLAY_CATEGORY_ICON_SIGNAL,
             OVERLAY_CATEGORY_ICON_WIFI,
             OVERLAY_CATEGORY_NAVBAR,
-            OVERLAY_CATEGORY_POWER_MENU);
+            OVERLAY_CATEGORY_POWER_MENU,
+            OVERLAY_CATEGORY_PROGRESS_BAR);
 
     /* Categories that need to be applied to the current user as well as the system user. */
     @VisibleForTesting
@@ -164,7 +171,8 @@ public class ThemeOverlayApplier implements Dumpable {
             OVERLAY_CATEGORY_ICON_ANDROID,
             OVERLAY_CATEGORY_ICON_SYSUI,
             OVERLAY_CATEGORY_NAVBAR,
-            OVERLAY_CATEGORY_POWER_MENU);
+            OVERLAY_CATEGORY_POWER_MENU,
+            OVERLAY_CATEGORY_PROGRESS_BAR);
 
     /* Allowed overlay categories for each target package. */
     private final Map<String, Set<String>> mTargetPackageToCategories = new ArrayMap<>();
@@ -208,6 +216,7 @@ public class ThemeOverlayApplier implements Dumpable {
         mCategoryToTargetPackage.put(OVERLAY_CATEGORY_ICON_WIFI, SYSUI_PACKAGE);
         mCategoryToTargetPackage.put(OVERLAY_CATEGORY_NAVBAR, SYSUI_PACKAGE);
         mCategoryToTargetPackage.put(OVERLAY_CATEGORY_POWER_MENU, SYSUI_PACKAGE);
+        mCategoryToTargetPackage.put(OVERLAY_CATEGORY_PROGRESS_BAR, ANDROID_PACKAGE);
 
         dumpManager.registerDumpable(TAG, this);
     }
