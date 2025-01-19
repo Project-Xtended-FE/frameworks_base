bc5aff79f77309fe63587a1147354adc2af3b1b1
minaripenguin <minaripenguin@users.noreply.github.com>
2025-01-19 18:07:41 +0800

    SystemUI: Do not update network layout when toggling hotspot
    reference issue: https://github.com/RisingTechOSS/issue_tracker/issues/88
    
    test: manual
    Change-Id: If80bb45c695532511c870b46613f3696292d448f
    Signed-off-by: Ghosuto <clash.raja10@gmail.com>

--- packages/SystemUI/src/com/android/systemui/qs/tiles/dialog/InternetDialogDelegateLegacy.java
+++ packages/SystemUI/src/com/android/systemui/qs/tiles/dialog/InternetDialogDelegateLegacy.java
@@ -360,7 +360,8 @@ public class InternetDialogDelegateLegacy implements
         mWifiRecyclerView.setLayoutManager(new LinearLayoutManager(context));
         mWifiRecyclerView.setAdapter(mAdapter);
 
-        updateDialogUI(getWifiNetworkContent(), true);
+        updateDialogUI(getWifiNetworkContent(), false);
+        setHotspotLayout();
     }
 
     @Override
@@ -450,7 +451,10 @@ public class InternetDialogDelegateLegacy implements
                 internetContent.mIsAirplaneModeEnabled ? View.VISIBLE : View.GONE);
 
         updateEthernet(internetContent);
-        setMobileDataLayout(internetContent);
+        
+        if (!shouldUpdateHotspot) {
+            setMobileDataLayout(internetContent);
+        }
 
         if (shouldUpdateHotspot) {
             setHotspotLayout();
