598c3bd332b50f4a212d4e7bd7a70c54e732071e
rmp22 <195054967+rmp22@users.noreply.github.com>
2025-03-30 20:32:45 +0800

    [Optimization] OomAdjuster: kill cached/restricted idle apps
    Change-Id: Iff00846ed19fb5987d6fba1f64e6a91fcab4ace9
    Signed-off-by: rmp22 <195054967+rmp22@users.noreply.github.com>
    Signed-off-by: Ghosuto <clash.raja10@gmail.com>

--- services/core/java/com/android/server/am/OomAdjuster.java
+++ services/core/java/com/android/server/am/OomAdjuster.java
@@ -4050,6 +4050,9 @@ public class OomAdjuster {
                     }
                 }
             }
+            if (uidRec.isIdle()) {
+                mProcessList.killAppIfBgRestrictedAndCachedIdleLocked(uidRec);
+            }
         }
         if (mService.mLocalPowerManager != null) {
             mService.mLocalPowerManager.finishUidChanges();
