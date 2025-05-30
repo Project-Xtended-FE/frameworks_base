1c6ccd4c35d0a3dbe6aa33904b570694448a9fd5
Beth Thibodeau <ethibodeau@google.com>
2025-05-30 13:58:48 -0500

    Update media target bounds after QS height changes
    Also add log info, clean up unused listeners in MediaViewController
    
    Bug: 404815434
    Bug: 415755300
    Test: verify media size after rotate, fold/unfold, close/open shade
    Flag: EXEMPT bugfix
    Change-Id: I4c945235397e57d85cbaeeaf0a7dff3e701ac5fe
    Signed-off-by: Ghosuto <clash.raja10@gmail.com>

--- packages/SystemUI/src/com/android/systemui/media/controls/ui/controller/MediaViewController.kt
+++ packages/SystemUI/src/com/android/systemui/media/controls/ui/controller/MediaViewController.kt
@@ -97,8 +97,6 @@ constructor(
 
     /** A listener when the current dimensions of the player change */
     lateinit var sizeChangedListener: () -> Unit
-    lateinit var configurationChangeListener: () -> Unit
-    lateinit var recsConfigurationChangeListener: (MediaViewController, TransitionLayout) -> Unit
     var locationChangeListener: (Int) -> Unit = {}
     private var firstRefresh: Boolean = true
     @VisibleForTesting private var transitionLayout: TransitionLayout? = null
@@ -296,18 +294,7 @@ constructor(
                             )
                         )
                     }
-                    if (SceneContainerFlag.isEnabled) {
-                        if (
-                            this@MediaViewController::recsConfigurationChangeListener.isInitialized
-                        ) {
-                            transitionLayout?.let {
-                                recsConfigurationChangeListener.invoke(this@MediaViewController, it)
-                            }
-                        }
-                    } else if (
-                        this@MediaViewController::configurationChangeListener.isInitialized
-                    ) {
-                        configurationChangeListener.invoke()
+                    if (!SceneContainerFlag.isEnabled) {
                         refreshState()
                     }
                 }
@@ -1005,7 +992,7 @@ constructor(
             // Let's squish the media player if our size was overridden
             result = squishViewState(result, state.squishFraction)
         }
-        logger.logMediaSize("update to carousel", result.width, result.height)
+        logger.logMediaSize("update to carousel (squish ${state?.squishFraction}", result.width, result.height)
         return result
     }
 
