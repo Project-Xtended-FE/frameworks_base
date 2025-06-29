e7bb3a106b9b7d558ca7ae673d2b8489321b09df
rmp22 <195054967+rmp22@users.noreply.github.com>
2025-06-29 21:32:00 +0800

    [Boost-framework] initial system integration
    Change-Id: I3b56ba3a058aecfd2d1e9d136c3528f79af34b99
    Signed-off-by: rmp22 <195054967+rmp22@users.noreply.github.com>
    Signed-off-by: Ghosuto <clash.raja10@gmail.com>

--- services/core/java/com/android/server/policy/PhoneWindowManager.java
+++ services/core/java/com/android/server/policy/PhoneWindowManager.java
@@ -440,6 +440,9 @@ public class PhoneWindowManager implements WindowManagerPolicy {
     private static final String ACTION_TORCH_OFF =
             "com.android.server.policy.PhoneWindowManager.ACTION_TORCH_OFF";
 
+    private static final long MEMORY_RELEASE_INTERVAL_MS = 10 * 60 * 1000L; // 10 minutes
+    private long lastMemoryReleaseTime = 0L;
+
     /**
      * Keyguard stuff
      */
@@ -6902,6 +6905,8 @@ public class PhoneWindowManager implements WindowManagerPolicy {
         if (mKeyguardDelegate != null) {
             mKeyguardDelegate.onStartedGoingToSleep(pmSleepReason);
         }
+
+        setLowPowerMode(true);
     }
 
     // Called on the PowerManager's Notifier thread.
@@ -6975,6 +6980,9 @@ public class PhoneWindowManager implements WindowManagerPolicy {
         }
 
         mPowerButtonLaunchGestureTriggered = false;
+        
+        releaseMemoryAtScreenOn();
+        setLowPowerMode(false);
     }
 
     // Called on the PowerManager's Notifier thread.
@@ -8605,6 +8613,26 @@ public class PhoneWindowManager implements WindowManagerPolicy {
             case AudioManager.RINGER_MODE_SILENT:
                 am.setRingerMode(AudioManager.RINGER_MODE_NORMAL);
                 break;
+        }
+    }
+
+    private void releaseMemoryAtScreenOn() {
+        long currentTime = System.currentTimeMillis();
+        if (lastMemoryReleaseTime == 0L || currentTime - lastMemoryReleaseTime > MEMORY_RELEASE_INTERVAL_MS) {
+            try {
+                ActivityManager.getService().releaseMemory(900, 20, false, false);
+                lastMemoryReleaseTime = currentTime;
+            } catch (RemoteException e) {
+            }
+        }
+    }
+    
+    private void setLowPowerMode(boolean enabled) {
+        boolean isBatterySaverOn = mPowerManager.isPowerSaveMode();
+        if (!isBatterySaverOn && mPowerManagerInternal != null) {
+            mPowerManagerInternal.setPowerMode(
+                android.hardware.power.Mode.LOW_POWER, enabled);
+            Log.d("Power Opt", (enabled ? "Enabling" : "Disabling") + " low power mode");
         }
     }
 }
