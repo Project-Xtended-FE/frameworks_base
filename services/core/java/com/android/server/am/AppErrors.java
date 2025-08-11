a945cae20af954abc33876aaa5db968e59429eed
Dil3mm4 <dil3mm4.dev@gmail.com>
2025-08-11 13:45:08 +0000

    Add product board in the log

--- services/core/java/com/android/server/am/AppErrors.java
+++ services/core/java/com/android/server/am/AppErrors.java
@@ -679,10 +679,12 @@ class AppErrors {
             }
 
             // Add paste content for Memochō option
-            String devfp = SystemProperties.get("ro.vendor.build.fingerprint", "");
+            String devFP = SystemProperties.get("ro.vendor.build.fingerprint", "");
             String xtendedVers = SystemProperties.get("ro.xtended.version", "");
+            String boardName = SystemProperties.get("ro.product.board", "");
             data.paste = "time: " + timeMillis + "\n" +
-            "device fp:" + devfp + "\n" +
+           "device fp:" + devFP + "\n" +
+            "product board:" + boardName + "\n" +
             "xtended vers:" + xtendedVers + "\n" +
             "msg: " + longMsg + "\n" +
             "stacktrace: " + stackTrace;
