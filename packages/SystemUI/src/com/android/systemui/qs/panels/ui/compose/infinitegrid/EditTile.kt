ecdee188db3f8542b6796c0ef501f4646c1b03d2
Abhay Singh Gill <abhaygill017@gmail.com>
2025-08-23 19:33:47 +0530

    SystemUI: Only use dual target color scheme for dual target tiles
    This seems to be the behaviour on QPR2.
    Also add haptics when performing a secondary/toggle click for legacy devices.
    
    Signed-off-by: Abhay Singh Gill <abhaygill017@gmail.com>

--- packages/SystemUI/src/com/android/systemui/qs/panels/ui/compose/infinitegrid/EditTile.kt
+++ packages/SystemUI/src/com/android/systemui/qs/panels/ui/compose/infinitegrid/EditTile.kt
@@ -1029,7 +1029,7 @@ fun EditTile(
                 .graphicsLayer { this.alpha = alpha },
     ) {
         // Icon
-        Box(Modifier.size(ToggleTargetSize)) {
+        Box(Modifier.size(CommonTileDefaults.IconSize)) {
             SmallTileContent(
                 iconProvider = { tile.icon },
                 color = colors.icon,
--- packages/SystemUI/src/com/android/systemui/qs/panels/ui/compose/infinitegrid/Tile.kt
+++ packages/SystemUI/src/com/android/systemui/qs/panels/ui/compose/infinitegrid/Tile.kt
@@ -214,7 +214,7 @@ fun Tile(
                     }
                 },
                 onLongClick = longClick,
-                accessibilityUiState = uiState.accessibilityUiState,
+                uiState = uiState,
                 iconOnly = iconOnly,
             ) {
                 val iconProvider: Context.() -> Icon = { getTileIcon(icon = icon) }
@@ -228,9 +228,7 @@ fun Tile(
                     val iconShape = RoundedCornerShape(TileCornerRadius)
                     val secondaryClick: (() -> Unit)? =
                         {
-                                hapticsViewModel?.setTileInteractionState(
-                                    TileHapticsViewModel.TileInteractionState.CLICKED
-                                )
+                                vibrator.vibrate(EFFECT_CLICK)
                                 tile.onSecondaryClick()
                             }
                             .takeIf { uiState.handlesSecondaryClick }
@@ -275,7 +273,7 @@ private fun TileExpandable(
 fun TileContainer(
     onClick: () -> Unit,
     onLongClick: (() -> Unit)?,
-    accessibilityUiState: AccessibilityUiState,
+    uiState: TileUiState,
     iconOnly: Boolean,
     content: @Composable BoxScope.() -> Unit,
 ) {
@@ -286,11 +284,11 @@ fun TileContainer(
                 .tileCombinedClickable(
                     onClick = onClick,
                     onLongClick = onLongClick,
-                    accessibilityUiState = accessibilityUiState,
+                    accessibilityUiState = uiState.accessibilityUiState,
                     iconOnly = iconOnly,
                 )
                 .sysuiResTag(if (iconOnly) TEST_TAG_SMALL else TEST_TAG_LARGE)
-                .thenIf(!iconOnly) { Modifier.largeTilePadding() }, // Icon tiles are center aligned
+                .thenIf(!iconOnly) { Modifier.largeTilePadding(uiState) }, // Icon tiles are center aligned
         content = content,
     )
 }
@@ -308,7 +306,7 @@ fun LargeStaticTile(
             .clip(RoundedCornerShape(TileCornerRadius))
             .background(colors.background)
             .height(TileHeight)
-            .largeTilePadding()
+            .largeTilePadding(uiState)
     ) {
         LargeTileContent(
             label = uiState.label,
@@ -336,7 +334,17 @@ fun tileHorizontalArrangement(): Arrangement.Horizontal {
 }
 
 fun Modifier.largeTilePadding(): Modifier {
-    return padding(start = TileStartPadding, end = TileEndPadding)
+    return padding(start = TileStartPadding + CommonTileDefaults.TileArrangementPadding, end = TileEndPadding)
+}
+
+fun Modifier.largeTilePadding(uiState: TileUiState): Modifier {
+    return this.then(
+        if (!uiState.handlesSecondaryClick) {
+            padding(start = TileStartPadding + CommonTileDefaults.TileArrangementPadding, end = TileEndPadding)
+        } else {
+            padding(start = TileStartPadding, end = TileEndPadding)
+        }
+    )
 }
 
 @Composable
@@ -440,7 +448,7 @@ private object TileDefaults {
     fun getColorForState(uiState: TileUiState, iconOnly: Boolean): TileColors {
         return when (uiState.state) {
             STATE_ACTIVE -> {
-                if (!iconOnly) {
+                if (uiState.handlesSecondaryClick && !iconOnly) {
                     activeDualTargetTileColors()
                 } else {
                     activeIconTileColors()
