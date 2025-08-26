926336a5f591a4509963058839f2f4d1143a8c20
Mill Chen <millchen@google.com>
2025-08-26 12:39:24 +0000

    [Expressive] Update slider content description
    The content description for SliderPreference is now set on
    holder.itemView instead of just the internal slider widget. When the
    slider content description is updated, notifyChanged is called to ensure
    the view is re-bound with the new description.
    
    Bug: 436524151
    Test: manual test with Talkback
    Flag: EXEMPT bugfix for accessibility
    Change-Id: I5a2a6dfb9e8f5d8f3f3504a9372c8f9e25261bb9
    Signed-off-by: Ghosuto <clash.raja10@gmail.com>

--- packages/SettingsLib/SliderPreference/src/com/android/settingslib/widget/SliderPreference.java
+++ packages/SettingsLib/SliderPreference/src/com/android/settingslib/widget/SliderPreference.java
@@ -278,11 +278,11 @@ public class SliderPreference extends Preference {
         }
         final CharSequence title = getTitle();
         if (!TextUtils.isEmpty(mSliderContentDescription)) {
-            mSlider.setContentDescription(mSliderContentDescription);
+            holder.itemView.setContentDescription(mSliderContentDescription);
         } else if (!TextUtils.isEmpty(title)) {
-            mSlider.setContentDescription(title);
+            holder.itemView.setContentDescription(title);
         } else {
-            mSlider.setContentDescription(null);
+            holder.itemView.setContentDescription(null);
         }
         mSlider.setValueFrom(mMin);
         mSlider.setValueTo(mMax);
@@ -488,8 +488,10 @@ public class SliderPreference extends Preference {
      * @see #getShowSliderValue()
      */
     public void setShowSliderValue(boolean showSliderValue) {
-        mShowSliderValue = showSliderValue;
-        notifyChanged();
+        if (showSliderValue != mShowSliderValue) {
+            mShowSliderValue = showSliderValue;
+            notifyChanged();
+        }
     }
 
     public void setLabelFormater(@Nullable LabelFormatter formater) {
@@ -521,9 +523,9 @@ public class SliderPreference extends Preference {
      * @param contentDescription The content description of the {@link Slider}
      */
     public void setSliderContentDescription(@Nullable CharSequence contentDescription) {
-        mSliderContentDescription = contentDescription;
-        if (mSlider != null) {
-            mSlider.setContentDescription(contentDescription);
+        if (!TextUtils.equals(contentDescription, mSliderContentDescription)) {
+            mSliderContentDescription = contentDescription;
+            notifyChanged();
         }
     }
 
