c2da6808b89006477e7c384926d4f9cc6cafd930
Riddle Hsu <riddlehsu@google.com>
2025-08-29 14:40:07 +0800

    Reduce blocking operation on display thread
    For example, thread A acquires wm lock 20ms and it posts a
    message to another thread B that also needs to enter wm lock.
    Then B will need to wait for the 20ms operation to finish.
    
    Currently the common cases are the message of task-layer-rank
    and idle-check. For rank, it can be done with deferred scope
    without posting a message. For idle-check, it can post a
    regular 10ms timeout rather than an immediate message if the
    activity is in a transition because when transition is finished,
    scheduleProcessStoppingAndFinishingActivitiesIfNeeded will also
    check idle.
    
    Bug: 422656135
    Test: Return to home from an app by pressing home key.
          Check trace about the timestamp of setProcessGroup.
    Change-Id: I9f455f1063ef04c049c5743eda9f6c175ec3a0ba

--- services/core/java/com/android/server/wm/ActivityRecord.java
+++ services/core/java/com/android/server/wm/ActivityRecord.java
@@ -5994,8 +5994,8 @@ final class ActivityRecord extends WindowToken {
                 case PAUSING:
                 case PAUSED:
                 case STARTED:
-                    addToStopping(true /* scheduleIdle */,
-                            canEnterPictureInPicture /* idleDelayed */, "makeInvisible");
+                    final boolean idleDelayed = canEnterPictureInPicture || inTransition();
+                    addToStopping(true /* scheduleIdle */, idleDelayed, "makeInvisible");
                     break;
 
                 default:
