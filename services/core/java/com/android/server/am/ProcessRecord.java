6e0243998ae95f8b8a270f0c005e37fde1942a13
lihaizhou <lihz21@motorola.corp-partner.google.com>
2025-09-02 11:20:16 +0800

    Avoid processRecord reuse during lmkd process killing.
    When lmkd kills a process due to memory issues, the killing process may take a long time due to high system load.
    If the user opens the app during this period, it may crash.
    
    Bug: 441222667
    Change-Id: Id71b1c064143f8fe039442181e43b3d58a0d4820
    Signed-off-by: lihaizhou <lihz21@motorola.corp-partner.google.com>

--- services/core/java/com/android/server/am/ProcessRecord.java
+++ services/core/java/com/android/server/am/ProcessRecord.java
@@ -287,6 +287,12 @@ public class ProcessRecord implements WindowProcessListener {
     private boolean mKilledByAm;
 
     /**
+     * True when proc has been triggered by lmkd, due to RAM.
+     * not indicate that the killing has been completed
+     */
+    private boolean mKilledByLmk;
+
+    /**
      * True once we know the process has been killed.
      */
     @CompositeRWLock({"mService", "mProcLock"})
@@ -1019,6 +1025,14 @@ public class ProcessRecord implements WindowProcessListener {
     @GuardedBy({"mService", "mProcLock"})
     void setKilledByAm(boolean killedByAm) {
         mKilledByAm = killedByAm;
+    }
+
+    boolean isKilledByLmk() {
+        return mKilledByLmk;
+    }
+
+    void setKilledByLmk(boolean killedByLmk) {
+        mKilledByLmk = killedByLmk;
     }
 
     @GuardedBy(anyOf = {"mService", "mProcLock"})
