3d92a764169dbcea73192c9d880569a5e83bac86
Abhay Singh Gill <abhaygill017@gmail.com>
2025-09-19 20:08:05 +0530

    SystemUI: Make large tile have same icon size as small tile
    Signed-off-by: Abhay Singh Gill <abhaygill017@gmail.com>

--- packages/SystemUI/src/com/android/systemui/qs/panels/ui/compose/infinitegrid/EditTile.kt
+++ packages/SystemUI/src/com/android/systemui/qs/panels/ui/compose/infinitegrid/EditTile.kt
@@ -1032,7 +1032,6 @@ fun EditTile(
     progress: () -> Float,
     colors: TileColors = EditModeTileDefaults.editTileColors(),
 ) {
-    val iconSizeDiff = CommonTileDefaults.IconSize - CommonTileDefaults.LargeTileIconSize
     val alpha by animateFloatAsState(if (tileState == TileState.GreyedOut) .4f else 1f)
     Row(
         horizontalArrangement = spacedBy(TileArrangementPadding),
@@ -1075,7 +1074,6 @@ fun EditTile(
                 iconProvider = { tile.icon },
                 color = colors.icon,
                 animateToEnd = true,
-                size = { CommonTileDefaults.IconSize - iconSizeDiff * progress() },
                 modifier = Modifier.align(Alignment.Center),
             )
         }
