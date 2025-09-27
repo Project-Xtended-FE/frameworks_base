c841f5f6cdc285292eb0c59610fc0f422a88a499
rmp22 <195054967+rmp22@users.noreply.github.com>
2025-09-27 15:01:24 +0800

    adding UxPerformance port
    Change-Id: I5265476de1751b3b37a9cdd182ba44245a118d8f
    Signed-off-by: rmp22 <195054967+rmp22@users.noreply.github.com>

--- services/core/java/com/android/server/display/DisplayPowerController.java
+++ services/core/java/com/android/server/display/DisplayPowerController.java
@@ -2332,6 +2332,7 @@ final class DisplayPowerController implements AutomaticBrightnessController.Call
         }
         
         AxExtServiceFactory.getAppUsageManager().setScreenState(isOff);
+        AxExtServiceFactory.getUxPerformance().setScreenState(isOff);
 
         // Return true if the screen isn't blocked.
         return mPendingScreenOnUnblocker == null
--- services/core/java/com/android/server/wm/ActivityTaskSupervisor.java
+++ services/core/java/com/android/server/wm/ActivityTaskSupervisor.java
@@ -56,6 +56,7 @@ import static android.view.WindowManager.TRANSIT_TO_FRONT;
 import static com.android.internal.protolog.WmProtoLogGroups.WM_DEBUG_STATES;
 import static com.android.internal.protolog.WmProtoLogGroups.WM_DEBUG_TASKS;
 import static com.android.internal.protolog.WmProtoLogGroups.WM_DEBUG_WINDOW_TRANSITIONS;
+import static com.android.server.wm.ActivityRecord.State.DESTROYED;
 import static com.android.server.wm.ActivityRecord.State.PAUSED;
 import static com.android.server.wm.ActivityRecord.State.PAUSING;
 import static com.android.server.wm.ActivityRecord.State.RESTARTING_PROCESS;
@@ -1570,6 +1571,14 @@ public class ActivityTaskSupervisor implements RecentTasks.Callbacks {
     void findTaskToMoveToFront(Task task, int flags, ActivityOptions options, String reason,
             boolean forceNonResizeable) {
         Task currentRootTask = task.getRootTask();
+        
+        Task focusedStack = mRootWindowContainer.getTopDisplayFocusedRootTask();
+        ActivityRecord top = focusedStack != null ? focusedStack.getTopNonFinishingActivity() : null;
+        if((top != null) && (top.getState() == DESTROYED)) {
+            AxExtServiceFactory.getUxPerformance().perfIOPrefetchStart(-1, top.packageName,
+                           top.info.applicationInfo.sourceDir.substring(0, top.info.applicationInfo.sourceDir.lastIndexOf('/')));
+        }
+
         if (currentRootTask == null) {
             Slog.e(TAG, "findTaskToMoveToFront: can't move task="
                     + task + " to front. Root task is null");
