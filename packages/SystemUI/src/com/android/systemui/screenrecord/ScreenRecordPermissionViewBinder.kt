e44a66a1ecbe0084c77d8fe236c7cd01f34c6c4a
Ido Ben-Hur <idoybh2@gmail.com>
2025-09-29 17:18:17 +0200

    SystemUI: Hide HEVC screen recording when no HW codec is available
    Change-Id: I1e5c265980f34a943b2466b1d84d57e009961f8d
    Signed-off-by: Ghosuto <clash.raja10@gmail.com>

--- packages/SystemUI/src/com/android/systemui/screenrecord/ScreenRecordPermissionViewBinder.kt
+++ packages/SystemUI/src/com/android/systemui/screenrecord/ScreenRecordPermissionViewBinder.kt
@@ -40,6 +40,8 @@ import android.widget.AdapterView
 import android.widget.ArrayAdapter
 import android.widget.CompoundButton
 import android.widget.Spinner
+import android.media.MediaCodecList
+import android.media.MediaFormat
 import androidx.annotation.LayoutRes
 import com.android.systemui.Prefs
 import com.android.systemui.mediaprojection.MediaProjectionCaptureTarget
@@ -207,6 +209,12 @@ class ScreenRecordPermissionViewBinder(
                 }
             }
         options.isLongClickable = false
+
+        // Disable HEVC when hardware accelerated codec is not available
+        if (!hasHevcHwEncoder()) {
+            Prefs.putInt(userContextProvider.userContext, PREF_HEVC, 0)
+            containerView.requireViewById<View>(R.id.show_hevc).visibility = GONE
+        }
 
         loadPrefs();
     }
@@ -411,5 +419,20 @@ class ScreenRecordPermissionViewBinder(
                     (!filterDeviceTypeFlag || it.type in RECORDABLE_DISPLAY_TYPES)
             }
         }
+    }
+
+    private fun hasHevcHwEncoder(): Boolean {
+        val mediaCodecList = MediaCodecList(MediaCodecList.REGULAR_CODECS)
+        for (codecInfo in mediaCodecList.codecInfos) {
+            if (!codecInfo.isEncoder || !codecInfo.isHardwareAccelerated) {
+                continue
+            }
+            for (type in codecInfo.supportedTypes) {
+                if (type.equals(MediaFormat.MIMETYPE_VIDEO_HEVC, ignoreCase = true)) {
+                    return true
+                }
+            }
+        }
+        return false
     }
 }
