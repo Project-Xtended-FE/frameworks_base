2754f37582958d649d40d99932c8c6938ac882c3
Ghosuto <clash.raja10@gmail.com>
2025-10-25 04:35:59 +0000

    Pulse: Add height control option
    Change-Id: Iee837e0eef2a725f2399c5754b3bf006d405fe00
    Signed-off-by: Ghosuto <clash.raja10@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -14149,6 +14149,11 @@ public final class Settings {
         public static final String PULSE_SHOW_ON_AMBIENT = "pulse_show_on_ambient";
 
         /**
+         * @hide
+         */
+        public static final String PULSE_HEIGHT_MULTIPLIER = "pulse_height_multiplier";
+
+        /**
          * Translucent notifications
          * @hide
          */
