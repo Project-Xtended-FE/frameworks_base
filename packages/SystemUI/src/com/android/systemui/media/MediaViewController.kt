ba78884a2e1805f70d15f2162fd4c7589c1816dd
Pranav Vashi <neobuddy89@gmail.com>
2025-11-02 11:51:51 +0530

    SystemUI: Send dozing state change value to listeners
    Change-Id: I946ffe37be5e748791d6d28fdaf525c2a51361b7
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: Ghosuto <clash.raja10@gmail.com>

--- packages/SystemUI/src/com/android/systemui/media/MediaViewController.kt
+++ packages/SystemUI/src/com/android/systemui/media/MediaViewController.kt
@@ -499,7 +499,7 @@ class MediaViewController @Inject constructor(
         }
     }
 
-    override fun onDozingChanged() {
+    override fun onDozingChanged(dozing: Boolean) {
         coroutineScope.launch {
             onMediaStateChanged()
         }
