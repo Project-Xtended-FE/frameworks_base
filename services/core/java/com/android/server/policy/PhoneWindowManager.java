cdc182dfcb86b521765011ccab3fb5260b34bca6
rmp22 <195054967+rmp22@users.noreply.github.com>
2025-11-02 17:40:41 +0800

    services: Unifying non-native/native double tap to doze
    unifying both non-native/native doze implementations for ease of maintaining this feature. non-native double tap to doze sends keycode wakeup which native doesnt do, the two settings does not interfere with each other and this gives a single preference to maintain for consistency, non-native devices just needs to define dummy config_dozeDoubleTapSensorType to enable this feature
    
    Change-Id: Ib95a2da43d2d8906ac4aa85c557d8283554cada2
    Signed-off-by: rmp22 <195054967+rmp22@users.noreply.github.com>
    Signed-off-by: Ghosuto <clash.raja10@gmail.com>

--- services/core/java/com/android/server/policy/PhoneWindowManager.java
+++ services/core/java/com/android/server/policy/PhoneWindowManager.java
@@ -541,6 +541,7 @@ public class PhoneWindowManager implements WindowManagerPolicy {
 
     // Double-tap-to-doze
     private boolean mDoubleTapToWake;
+    private boolean mDoubleTapToWakeNative;
     private boolean mDoubleTapToDoze;
     private boolean mNativeDoubleTapToDozeAvailable;
 
@@ -2740,7 +2741,7 @@ public class PhoneWindowManager implements WindowManagerPolicy {
                 res.getBoolean(com.android.internal.R.bool.config_wakeOnDpadKeyPress);
 
         // Double-tap-to-doze
-        mNativeDoubleTapToDozeAvailable = !android.text.TextUtils.isEmpty(
+        mNativeDoubleTapToDozeAvailable = !TextUtils.isEmpty(
                 mContext.getResources().getString(R.string.config_dozeDoubleTapSensorType));
 
         // Init display burn-in protection
@@ -3555,6 +3556,8 @@ public class PhoneWindowManager implements WindowManagerPolicy {
         final boolean kidsModeEnabled;
         
         // Double-tap-to-doze
+        mDoubleTapToWakeNative = Settings.Secure.getIntForUser(resolver,
+                Settings.Secure.DOZE_DOUBLE_TAP_GESTURE, 0, UserHandle.USER_CURRENT) == 1;
         mDoubleTapToWake = Settings.Secure.getIntForUser(resolver,
                 Settings.Secure.DOUBLE_TAP_TO_WAKE, 0, UserHandle.USER_CURRENT) == 1;
         mDoubleTapToDoze = Settings.Secure.getIntForUser(resolver,
@@ -6384,7 +6387,7 @@ public class PhoneWindowManager implements WindowManagerPolicy {
                         KeyGestureEvent.KEY_GESTURE_TYPE_WAKEUP);
                 result &= ~ACTION_PASS_TO_USER;
                 // Double-tap-to-doze
-                 if (mDoubleTapToWake && mDoubleTapToDoze && !mNativeDoubleTapToDozeAvailable) {
+                 if ((mDoubleTapToWake || mDoubleTapToWakeNative) && mDoubleTapToDoze && mNativeDoubleTapToDozeAvailable) {
                     isWakeKey = false;
                     if (!down) {
                         Intent intent = new Intent("com.android.systemui.doze.pulse");
