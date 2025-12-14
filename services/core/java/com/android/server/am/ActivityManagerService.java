be830dd3ac9e56f61bac765804f6d663c8949d6d
rmp22 <195054967+rmp22@users.noreply.github.com>
2025-12-14 17:01:01 +0800

    services: Deprecating task profiler
    Change-Id: I0004c7ead912d55155409d512fbacf24834af1e1
    Signed-off-by: Ghosuto <clash.raja10@gmail.com>

--- services/core/java/com/android/server/am/ActivityManagerService.java
+++ services/core/java/com/android/server/am/ActivityManagerService.java
@@ -775,8 +775,6 @@ public class ActivityManagerService extends IActivityManager.Stub
 
     private AccessCheckDelegateHelper mAccessCheckDelegateHelper;
     
-    private final TaskProfiler mTaskProfiler = new TaskProfiler();
-
     /**
      * Uids of apps with current active camera sessions.  Access synchronized on
      * the IntArray instance itself, and no other locks must be acquired while that
@@ -5275,7 +5273,6 @@ public class ActivityManagerService extends IActivityManager.Stub
 
             // Start PSI monitoring in LMKD if it was skipped earlier.
             ProcessList.startPsiMonitoringAfterBoot();
-            mTaskProfiler.initTaskProfiles();
             
             mHandler.postDelayed(() -> {
                 SystemProperties.set("persist.sys.axion_boot_completed", "1");
